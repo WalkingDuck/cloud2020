82eb83f70b87d319d5e9b17ad59d3171c2e8208b
WalkingDuck <1164199234@qq.com>
2020-12-30 17:33:41 +0800

    手动实现Ribbon轮询策略负载均衡

--- cloud-consumer-order80/src/main/java/com/wtw/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/wtw/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.wtw.springcloud;
 
+import com.wtw.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class) // name要大写
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/wtw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/wtw/springcloud/controller/OrderController.java
@@ -2,15 +2,22 @@ package com.wtw.springcloud.controller;
 
 import com.wtw.springcloud.entities.CommonResult;
 import com.wtw.springcloud.entities.Payment;
+import com.wtw.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
+@EnableDiscoveryClient
 @Slf4j
 public class OrderController {
 
@@ -18,6 +25,12 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
@@ -29,5 +42,18 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         CommonResult commonResult = restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
         return commonResult;
+    }
+
+    @GetMapping("/consumer/payment/lb")
+    public String getPaymentLB() {
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if(serviceInstances == null || serviceInstances.size() == 0) {
+            return null;
+        }
+
+        ServiceInstance instance = loadBalancer.instances(serviceInstances);
+        URI uri = instance.getUri();
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
 }
