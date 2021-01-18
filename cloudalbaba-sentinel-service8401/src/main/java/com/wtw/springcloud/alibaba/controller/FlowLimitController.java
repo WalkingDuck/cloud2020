192f6c21f621ac289abc002853abcbb013626155
WalkingDuck <1164199234@qq.com>
2021-01-18 17:33:34 +0800

    sentinel兜底+整合

--- cloudalbaba-sentinel-service8401/src/main/java/com/wtw/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalbaba-sentinel-service8401/src/main/java/com/wtw/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,10 @@
 package com.wtw.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,15 @@ public class FlowLimitController {
 
         int a = 10 / 0;
         return "----------testD----------";
+    }
+
+    @RequestMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1, @RequestParam(value = "p2", required = false) String p2) {
+        return "testHotKey Success!";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException b1) {
+        return "deal_testHotKey Success!";
     }
 }
---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/wtw/springcloud/alibaba/service/PaymentFallbackService.java
@@ -0,0 +1,15 @@
+package com.wtw.springcloud.alibaba.service;
+
+import com.wtw.springcloud.entities.CommonResult;
+import com.wtw.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentService
+{
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id)
+    {
+        return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
+    }
+}
